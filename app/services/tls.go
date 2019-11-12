0a195ee85fa3006758bc061be508f6dc8187de70
Tomy Smith <me@tomm.us>
2019-11-12 13:11:02 +0000

    tidy tls file

--- app/services/tls.go
+++ app/services/tls.go
@@ -14,18 +14,9 @@ type TLSServiceProvider struct {
 	service.BaseProvider
 }
 
-// certificateDirectory returns the path of a file in the config
-// `certificate_directory` property.
-func (p TLSServiceProvider) certificateDirectory(file string) string {
-	return config.Get("app", "certificate_directory") + "/" + file
-}
-
-// Register checks the application's environment and generates
-// some TLS certificates if it is not running in production.
 func (p TLSServiceProvider) Register(b *di.Builder) {
-	// If the app environment is in production, then instead of
-	// creating self-signed certificates, we instead use the
-	// amazing acme/autocert package to interact with LE.
+	// If the app environment is in production, we the acme/autocert
+	// package is added to the container to interact with LE.
 	if config.Get("app", "environment") == "production" {
 		b.Add(di.Def{
 			Name: "autocert",
@@ -38,7 +29,6 @@ func (p TLSServiceProvider) Register(b *di.Builder) {
 	}
 }
 
-// buildAutocertManager
 func (p TLSServiceProvider) buildAutocertManager() *autocert.Manager {
 	return &autocert.Manager{
 		Prompt: autocert.AcceptTOS,
