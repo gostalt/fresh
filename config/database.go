a2614b753499423d0dc5c46bb4ad47b0481763da
Tomy Smith <me@tomm.us>
2019-11-15 13:44:29 +0000

    use memory database when using sqlite

--- config/database.go
+++ config/database.go
@@ -3,7 +3,17 @@ package config
 func database(env map[string]string) map[string]string {
 	return map[string]string{
 		"driver":              env["DB_DRIVER"],
-		"string":              env["DB_CONNECTION_STRING"],
+		"string":              getDbConnectionString(env),
 		"migration_directory": "./database/migrations",
 	}
+}
+
+func getDbConnectionString(env map[string]string) string {
+	// If the database driver is sqlite3, then the connection string
+	// should just be a memory connection - sqlite is just for testing.
+	if env["DB_DRIVER"] == "sqlite3" {
+		return "file:ent?mode=memory&cache=shared&_fk=1"
+	}
+
+	return env["DB_CONNECTION_STRING"]
 }
