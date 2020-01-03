836ab04b1bc64f62a1105b679da00876c789a6c5
Tom Smith <me@tomm.us>
2020-01-03 14:47:00 +0000

    ADD RedirectIfAuthenticated middleware

--- app/http/middleware/RedirectIfAuthenticated.go
+++ app/http/middleware/RedirectIfAuthenticated.go
@@ -1,8 +1,31 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
 
+	"github.com/gorilla/sessions"
+	"github.com/sarulabs/di/v2"
+)
+
+// RedirectIfAuthenticated provides the "inverse" of the Authenticate
+// middleware: if a user is already logged in, they are prevented
+// from accessing a page.
+//
+// This is used by Gostalt to prevent authenticated users from
+// visiting the login and register pages.
 func RedirectIfAuthenticated(next http.Handler) http.Handler {
-	// TODO: Implement
-	return next
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			store := di.Get(r, "session").(*sessions.CookieStore)
+			sess, err := store.Get(r, "gostalt")
+
+			user := sess.Values["user"]
+			if user == "" || user == nil || err != nil {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			http.Redirect(w, r, "/home", 302)
+		},
+	)
 }
