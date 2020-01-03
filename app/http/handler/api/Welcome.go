380f727ffe8d827f8347fe823ace8d9152d784de
Tom Smith <me@tomm.us>
2020-01-03 13:52:07 +0000

    REMOVE unneeded code from default api route

--- app/http/handler/api/Welcome.go
+++ app/http/handler/api/Welcome.go
@@ -1,33 +1,10 @@
 package api
 
 import (
-	"fmt"
-	"gostalt/app/entity"
 	"net/http"
-
-	"github.com/gostalt/validate"
-	"github.com/sarulabs/di/v2"
 )
 
 func Welcome(w http.ResponseWriter, r *http.Request) {
-	client := di.Get(r, "entity-client").(*entity.Client)
-	name := r.Form.Get(":name")
-
-	msgs, err := validate.Check(r, validate.Rule{Param: ":name", Check: validate.MinLength, Options: validate.Options{"length": 6}})
-	if err != nil {
-		validate.Respond(w, msgs)
-		return
-	}
-
-	u, err := client.User.
-		Create().
-		SetName(name).
-		Save(r.Context())
-	if err != nil {
-		w.Write([]byte(`{"error": "user could not be created"}`))
-		return
-	}
-
-	w.Write([]byte(fmt.Sprintf(`{"greeting": "Hello, %s! From Gostalt."}`, u.Name)))
+	w.Write([]byte(`{"greeting": "Hello from Gostalt."}`))
 	return
 }
--- routes/api.go
+++ routes/api.go
@@ -10,5 +10,5 @@ import (
 )
 
 var API = route.Collection(
-	route.Get("/create/{name}", http.HandlerFunc(api.Welcome)),
-).Prefix("api").Middleware(mw.AddJSONContentTypeHeader, mw.AddURIParametersToRequest)
+	route.Get("/", http.HandlerFunc(api.Welcome)),
+).Prefix("api").Middleware(mw.AddJSONContentTypeHeader)
